1a3436fa91264d70d390cc1e29ae350a086d7773
Denis Oblogin <ukrbublik@gmail.com>
2017-09-20 23:52:26 +0300

    .

--- docker-create.sh
+++ docker-create.sh
@@ -3,7 +3,7 @@
 docker build -f seta.Dockerfile  --build-arg NODE_ENV='stage' -t seta-base .
 docker tag seta-base ukrbublik.docker/seta-base:latest
 
-docker network create -d bridge --subnet 172.21.0.0/16 seta_nw | echo 'Network not created'
+docker network create -d bridge --subnet 173.21.0.0/16 seta_nw | echo 'Network not created'
 docker run -p 8081:80 --name=container-seta-node --network=seta_nw  -e NODE_ENV='stage' -it -d seta-base
 
 
